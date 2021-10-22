2e483bf17faa3d44e8fa067dfe2cfa29eb836bf1
erwinfattah <68496276+erwinfattah@users.noreply.github.com>
2021-10-22 23:36:49 +0700

    Edit Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,48 +1,65 @@
 import React from 'react'
+import { ProgressBar, Container} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from '../template/Header';
+import style from '../style'
 
 
 
 const Profile = () => {
+
+    const classes = style()
     
     return (
         <React.Fragment>
+            <Header />
             <div className="profile"> 
-                <img src="" />
-                <h4>Halo-Halo</h4>
+                <img src="../../assets/Isom.jpg" id="img-person"/>
                 <div className="person-data">
-                    <h4 id="person-name">Ian Sommerville</h4>
+                    <ul className="detail-person-data">
+                        <li><h4 id="person-name">Ian Sommerville</h4></li>
+                        <li><h4>Balance</h4></li>
+                    </ul>
+                    <h4>Level</h4>
+                    <ProgressBar now={80} />
                     {/*Nanti kalau backend sudah set: ngambil data level*/}
                     {/*Nanti kalau backend sudah set: balance*/}
                 </div>
             </div>
 
-            <h3>Course yang telah diambil</h3>
-            <div className="list-enrolled-course">
-                <ul>
-                    <li>Backend</li>
-                    <li>Frontend</li>
-                    <li>Fullstack</li>
-                </ul>
+            <h4 id="label-course">Course yang telah diambil</h4>
+            <div>
+                <Container fluid  className="list-enrolled-course">
+                    <ul>
+                        <li>Backend</li>
+                        <li>Frontend</li>
+                        <li>Fullstack</li>
+                    </ul>
+                </Container>
             </div>
 
-            <h3>Project yang telah diambil</h3>
-            <div className="list-taken-project" >
-                <ul>
-                    <li>Website UMKM</li>
-                    <li>Website Kampus</li>
-                    <li>Mockup sinar damai App</li>
-                    {/*Nanti kalau backend sudah set: ngambil data project yang telah diambil*/}
-                </ul>
+            <h4 id="label-taken-project">Project yang telah diambil</h4>
+            <div >
+                <Container fluid  className="list-taken-project" >
+                    <ul>
+                        <li>Website UMKM</li>
+                        <li>Website Kampus</li>
+                        <li>Mockup sinar damai App</li>
+                        {/*Nanti kalau backend sudah set: ngambil data project yang telah diambil*/}
+                    </ul>
+                </Container>
             </div>
 
-            <h3>Project yang diajukan</h3>
-            <div className="list-added-project">
-                <ul>
-                    <li>Website untuk Pelabuhan</li>
-                    <li>Website Kampus</li>
-                    <li>Mockup sinar damai App</li>
-                    {/*Nanti kalau backend sudah set: ngambil data project yang diajukan*/}
-                </ul>
+            <h4 id="label-added-project">Project yang diajukan</h4>
+            <div >
+                <Container fluid className={classes.list_enrolled_course}>
+                    <ul>
+                        <li>Website untuk Pelabuhan</li>
+                        <li>Website Kampus</li>
+                        <li>Mockup sinar damai App</li>
+                        {/*Nanti kalau backend sudah set: ngambil data project yang diajukan*/}
+                    </ul>
+                </Container>
             </div>
         </React.Fragment>
     )
