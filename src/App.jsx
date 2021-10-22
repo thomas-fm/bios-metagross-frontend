9e26c1dc2c2818f4a2e7383f6340e1410fb82da8
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2021-10-22 23:39:51 +0700

    fix app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
-<<<<<<< HEAD
-import React from 'react'
-import './index.css'
-import './pages/Profile'
-import Profile from './pages/Profile'
-=======
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Dashboard from './pages/dashboard'
->>>>>>> 0339c5eadffce2547a16fd3567b0a61f4d3a9100
 
 
 function App() {
  
   return (
-<<<<<<< HEAD
-    <React.Fragment>
-      <Profile />
-    </React.Fragment>
-=======
     <Dashboard/>
->>>>>>> 0339c5eadffce2547a16fd3567b0a61f4d3a9100
   )
 }
 
