28ad39acea5938ced785c5b9184b594aa9e4b703
afiffahreza <afiffahreza3@gmail.com>
2021-10-22 22:26:37 +0700

    Add SubmitProject file tp kosong

---
+++ src/pages/SubmitProject.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SubmitProject = () => {
+    return (    
+        <React.Fragment>
+            <div className="course-detail">
+                <img src=""/>
+            </div>
+            <div>
+                <h4>Free</h4>
+                <button>Enroll</button>
+            </div>
+        </React.Fragment>
+    )
+}
+
+export default SubmitProject
